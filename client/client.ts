25d89ef039ba1442b4749867e4f1d070f341568e
alejandrojimenezrios56-hash <alejandrojimenezrios56@gmail.com>
2026-03-08 21:20:27 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,4 +1,5 @@
-// Client
-console.log("My address:", pg.wallet.publicKey.toString());
+// Cliente 
+
+console.log("Client wallet address:", pg.wallet.publicKey.toString());
 const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+console.log(`Client balance available for car rental: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
